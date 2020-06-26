a4e9e18b584f3fcecde4ef9d7189b7e6f0c265ff
Erik Slovak <erik.slovak@technologystudio.sk>
2020-06-26 18:43:08 +0200

    add isInitialNavigationOptions

---
+++ packages/@txo-peer-dep/react-conditional-navigation/src/Api/NavigationOptionsHelper.js
@@ -0,0 +1,22 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2020-06-26T18:06:72+02:00
+ * @Copyright: Technology Studio
+ * @flow
+**/
+
+import type { NavigationScreenConfig } from 'react-navigation'
+
+export const isInitialNavigationOptions = <OPTIONS: Object>(navigationOptions: NavigationScreenConfig<OPTIONS>): NavigationScreenConfig<OPTIONS> => (params) => {
+  switch (typeof navigationOptions) {
+    case 'function': return {
+      ...navigationOptions(params),
+      gesturesEnabled: !params.navigation.state.isInitial,
+    }
+    default: return {
+      ...navigationOptions,
+      ...params,
+      gesturesEnabled: !params.navigation.state.isInitial,
+    }
+  }
+}
