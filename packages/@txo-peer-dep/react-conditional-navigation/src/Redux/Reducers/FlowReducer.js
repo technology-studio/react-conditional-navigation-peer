d5d9eb6439fd2c319e4fd27b4fc6b143cbd9d41a
Rostislav Šimoník <rostislav.simonik@technologystudio.sk>
2020-07-29 14:02:01 +0000

    Simplify validation logic for condition.

--- packages/@txo-peer-dep/react-conditional-navigation/src/Redux/Reducers/FlowReducer.js
+++ packages/@txo-peer-dep/react-conditional-navigation/src/Redux/Reducers/FlowReducer.js
@@ -50,10 +50,8 @@ export const abstractFlowRedurerFactory = <STATE: NavigationState, ROOT_STATE>(
     while (getStateCurrentRouteKey(previousState) !== getStateCurrentRouteKey(currentState)) {
       log.debug(logPrefix + ': STATE ITERATION', { previousState, currentState })
       const conditionalNavigation: ?ConditionalNavigationState = getConditionalNavigation(currentState)
-      const flowConditionKey = action?.flowConditionKey || null
-      const isFlowConditionKeyEqualOrMissing = flowConditionKey
-        ? flowConditionKey === conditionalNavigation?.condition.key
-        : true
+      const flowConditionKey = action?.flowConditionKey
+      const isFlowConditionKeyEqualOrMissing = flowConditionKey == null || flowConditionKey === conditionalNavigation?.condition.key
       if (currentState && conditionalNavigation && isFlowConditionKeyEqualOrMissing) {
         log.debug(logPrefix + ': DETECTED CONDITIONAL NAVIGATION', { currentState, conditionalNavigation })
         const { index, routes } = currentState
