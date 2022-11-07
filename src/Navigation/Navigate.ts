ff4f5fe080e939e0550325fde9d206ce32589168
Erik Slovak <erik.slovak@technologystudio.sk>
2022-11-07 11:36:45 +0100

    feat: create onActionFactory with Navigate action creator

---
+++ src/Navigation/Navigate.ts
@@ -0,0 +1,117 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2022-11-02T14:11:60+01:00
+ * @Copyright: Technology Studio
+**/
+
+import type {
+  NavigationAction,
+  NavigationState,
+  PartialState,
+} from '@react-navigation/native'
+import { last } from '@txo/functional'
+import { Log } from '@txo/log'
+
+import {
+  conditionalNavigationManager,
+} from '../Api/ConditionalNavigationManager'
+import type { Condition } from '../Model/Types'
+import {
+  getActiveLeafNavigationNode,
+  getActiveScreenPath,
+  getExistingNavigationNodeByRouteName,
+  getNavigationPathFromAction,
+} from '../Api/NavigationUtils'
+
+const log = new Log('app.Modules.ReactConditionalNavigation.Navigation.Navigate')
+
+const VOID = 'void'
+
+export const navigateActionCreator = ({
+  action,
+  getState,
+  nextOnAction,
+  originalOnAction,
+  restArgs,
+  screenConditionsMap,
+  setState,
+}: {
+  action: NavigationAction,
+  getState: () => NavigationState | PartialState<NavigationState> | undefined,
+  nextOnAction: (...args: any) => boolean,
+  originalOnAction: (...args: any) => boolean,
+  restArgs: any[],
+  screenConditionsMap: Record<string, Condition[]>,
+  setState: (state: NavigationState | PartialState<NavigationState> | undefined) => void,
+}): boolean => {
+  const {
+    flow,
+    payload,
+    reset,
+    skipConditionalNavigation,
+  } = action
+  const state = getState()
+
+  const currentActiveScreenPath = getActiveScreenPath(state)
+  const desiredScreenPath = [
+    ...currentActiveScreenPath ?? [],
+    ...getNavigationPathFromAction(action) ?? [],
+  ]
+  const finalScreenName = last(desiredScreenPath)
+
+  log.debug('default navigate', { action, state, currentActiveScreenPath })
+  if (!skipConditionalNavigation) {
+    const conditions = desiredScreenPath?.reduce<Condition[] | undefined>((conditions, screenName) => {
+      const screenConditions = screenConditionsMap[screenName]
+      return screenConditions?.length > 0 && !conditions
+        ? screenConditions
+        : conditions
+    }, undefined)
+    log.debug('default navigate - conditions', { conditions, currentActiveScreenPath, screenConditionsMap })
+    if (conditions && state) {
+      const resolveConditionsResult = conditionalNavigationManager.resolveConditions(conditions, action, state)
+      log.debug('N: RESOLVE CONDITIONS RESULT', { conditions, resolveConditionsResult, action, _conditionToResolveCondition: conditionalNavigationManager._conditionToResolveCondition })
+      if (resolveConditionsResult && state) {
+        const activeLeafNavigationNode = getActiveLeafNavigationNode(state)
+        activeLeafNavigationNode.conditionalNavigation = resolveConditionsResult
+        // TODO: figure out how to inject isInitial to state
+        log.debug('N: RESOLVE CONDITIONS RESULT - SET STATE', { state, resolveConditionsResult })
+        return nextOnAction(resolveConditionsResult.navigationAction, ...restArgs)
+      }
+    }
+  }
+
+  if (reset) {
+    log.debug('NAVIGATE WITH RESET')
+    const { reset, ...paramsWithoutReset } = payload?.params ?? {}
+    const newState = {
+      index: 0,
+      routes: [
+        { name: finalScreenName, params: paramsWithoutReset },
+      ],
+    }
+    setState(newState)
+    // TODO: figure out how to inject isInitial to state
+    return true
+  }
+
+  if (flow) {
+    const route = state && typeof state.index === 'number' ? state.routes[state.index] : undefined
+    if (route) {
+      route.conditionalNavigation = {
+        condition: { key: VOID },
+        postponedAction: null,
+        logicalTimestamp: conditionalNavigationManager.getAndIncrementLogicalClock(),
+        previousState: JSON.parse(JSON.stringify(state)),
+      }
+    }
+  }
+
+  const destinationNode = getExistingNavigationNodeByRouteName(state, finalScreenName)
+  if (destinationNode?.conditionalNavigation) {
+    destinationNode.conditionalNavigation = undefined
+  }
+
+  // TODO: figure out how to inject isInitial to state
+  return originalOnAction(action, ...restArgs)
+}
---
+++ src/Navigation/OnActionFactory.ts
@@ -0,0 +1,51 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2022-10-28T09:10:38+02:00
+ * @Copyright: Technology Studio
+**/
+
+import type {
+  NavigationState,
+  PartialState,
+} from '@react-navigation/native'
+import { Log } from '@txo/log'
+
+import type {
+  ActionCreatorAttributes,
+  Condition,
+} from '../Model/Types'
+
+import { navigateActionCreator } from './Navigate'
+
+const log = new Log('app.Modules.ReactConditionalNavigation.Navigation.onActionFactory')
+
+type OnActionFactoryAttributes = {
+  getState: () => NavigationState | PartialState<NavigationState> | undefined,
+  nextOnAction: (...args: any) => boolean,
+  screenConditionsMap: Record<string, Condition[]>,
+  setState: (state: NavigationState | PartialState<NavigationState> | undefined) => void,
+}
+
+export const onActionFactory = (onAction: any) => (attributes: OnActionFactoryAttributes, ...args: any): boolean => {
+  const {
+    nextOnAction,
+    screenConditionsMap,
+    getState,
+    setState,
+  } = attributes
+  const [action, ...restArgs] = args
+
+  const { type } = action ?? {}
+  log.debug('N: onAction', { screenConditionsMap, action })
+  const actionCreatorAttributes: ActionCreatorAttributes = { action, getState, setState, nextOnAction, originalOnAction: onAction, restArgs, screenConditionsMap }
+  const actionCreatorMap = {
+    NAVIGATE: navigateActionCreator,
+  }
+
+  const actionCreator = actionCreatorMap[type]
+
+  const result = actionCreator ? actionCreator(actionCreatorAttributes) : onAction(...args)
+  log.debug('N: onAction - action creator result', { result })
+
+  return result
+}
