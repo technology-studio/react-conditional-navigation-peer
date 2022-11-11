4a09fafa0aef6f3e1df86456781ddc3935ac29d9
Erik Slovak <erik.slovak@technologystudio.sk>
2022-11-11 11:34:45 +0100

    chore: update onActionFactory with correct types

--- src/Navigation/OnActionFactory.ts
+++ src/Navigation/OnActionFactory.ts
@@ -4,29 +4,24 @@
  * @Copyright: Technology Studio
 **/
 
-import type {
-  NavigationState,
-  PartialState,
-} from '@react-navigation/native'
 import { Log } from '@txo/log'
 
 import type {
   ActionCreatorAttributes,
-  Condition,
+  OnAction,
+  OnActionFactoryAttributes,
 } from '../Model/Types'
 
+// import { backActionCreator } from './Back'
+// import {
+//   cancelFlowActionCreator,
+//   finishFlowAndContinueActionCreator,
+// } from './Flow'
 import { navigateActionCreator } from './Navigate'
 
 const log = new Log('app.Modules.ReactConditionalNavigation.Navigation.onActionFactory')
 
-type OnActionFactoryAttributes = {
-  getState: () => NavigationState | PartialState<NavigationState> | undefined,
-  nextOnAction: (...args: any) => boolean,
-  screenConditionsMap: Record<string, Condition[]>,
-  setState: (state: NavigationState | PartialState<NavigationState> | undefined) => void,
-}
-
-export const onActionFactory = (onAction: any) => (attributes: OnActionFactoryAttributes, ...args: any): boolean => {
+export const onActionFactory = (onAction: OnAction) => (attributes: OnActionFactoryAttributes, ...args: Parameters<OnAction>): boolean => {
   const {
     nextOnAction,
     screenConditionsMap,
@@ -39,9 +34,12 @@ export const onActionFactory = (onAction: any) => (attributes: OnActionFactoryAt
   log.debug('N: onAction', { screenConditionsMap, action })
   const actionCreatorAttributes: ActionCreatorAttributes = { action, getState, setState, nextOnAction, originalOnAction: onAction, restArgs, screenConditionsMap }
   const actionCreatorMap = {
+    // CANCEL_FLOW: cancelFlowActionCreator,
+    // FINISH_FLOW_AND_CONTINUE: finishFlowAndContinueActionCreator,
+    // GO_BACK: backActionCreator,
     NAVIGATE: navigateActionCreator,
   }
 
-  const actionCreator = actionCreatorMap[type]
+  const actionCreator = type in actionCreatorMap ? actionCreatorMap[type as keyof typeof actionCreatorMap] : undefined
   return actionCreator ? actionCreator(actionCreatorAttributes) : onAction(...args)
 }
